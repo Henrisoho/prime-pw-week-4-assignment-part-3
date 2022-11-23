a6f6d36a32da6e8e7551aeb2f4949430351085db
henrisoho <bakingwithmymother@gmail.com>
2022-11-23 08:50:06 -0600

    empty basket function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -17,9 +17,20 @@ console.log(basket[0])// test basket
 //item list function
 function listItems(){
     for (var i = 0; i < basket.length; i++){
-        console.log(basket[i]);
+    console.log(basket[i]);
     }
 }//end function list item
 
 console.log(listItems());//test listItems
 
+//function that empties the basket
+function empty(){
+    while(basket.length > 0) {
+    basket.pop();
+    return 'done';
+}
+}//end function empty
+//i was going to use .splice but it returns a copy with the items removed and since the array is mentioned in other functions i want them to actually be removed
+
+console.log(empty())//test empty
+console.log(basket)// test empty

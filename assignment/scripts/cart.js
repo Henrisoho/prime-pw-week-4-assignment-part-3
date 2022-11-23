9f1174edc1bdd2e05cdf4669e532af201fd32ce6
henrisoho <bakingwithmymother@gmail.com>
2022-11-23 08:26:42 -0600

    basket add funtion

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//basket
+let basket = [];
+
+// add item function
+function addItem(item){
+    basket.push(item);
+    return true;
+}// end add item
+
+console.log(addItem('tissue'));//test add item
+console.log(basket[0])// test basket
